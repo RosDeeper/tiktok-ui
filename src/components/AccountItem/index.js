bc891272358b43398254d38d18e54d171ecd6b89
RosDeeper <hoanggg237@gmail.com>
2023-09-22 16:22:28 +0700

    UI Header #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons"
+import classNames from "classnames/bind"
+import styles from "./AccountItem.module.scss"
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avavtar')} src="https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/1253dc15a05d70ebb29cb35c5f1ff0e4.jpeg?x-expires=1695542400&x-signature=mwDInPgjPkoZBz%2BVVAj0bI3mSEc%3D" alt=""/>
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>RosDeeper</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCircleCheck}/>
+                </h4>
+                <span className={cx('username')}>Pham Ngoc Hoang</span>
+            </div>
+        </div>
+    )
+}
+
+export default AccountItem
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,30 +1,54 @@
+import { useState } from 'react'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import Tippy from '@tippyjs/react/headless'
 
 import styles from './Header.module.scss'
 import images from '~/assets/images'
+import { Wrapper as PopperWrapper } from '~/components/Popper'
+import AccountItem from '~/components/AccountItem'
 
 const cx = classNames.bind(styles)
 
 function Header() {
+    const [searchResults, setSearchResults] = useState([1])
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <div className={cx('logo')}>
                     <img src={images.logo} alt=''/>
                 </div>
-                <div className={cx('search')}>
-                    <input placeholder="Search...." spellCheck={false}/> 
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark}/>
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
+
+                <Tippy 
+                    interactive
+                    visible={searchResults.length > 0}
+                    render={attrs => (
+                        <div  className={cx('search-result')} tabIndex='-1' {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-label')}>Accounts</h4>
+                                <AccountItem/>
+                                <AccountItem/>
+                                <AccountItem/>
+                                <AccountItem/>
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder='Search....' spellCheck={false}/>
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark}/>
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner}/>
                     
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                    </button>
-                </div>
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                        </button>
+                    </div>
+                </Tippy>
+
                 <div className={cx('action')}></div>
             </div>
         </header>
